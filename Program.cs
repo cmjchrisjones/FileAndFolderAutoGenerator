6932427a1886193e091a7b3ea6befa6721c0bca6
Chris Jones <cmj.chris.jones@googlemail.com>
2023-10-08 23:35:27 +0100

    Added in nested folders and randomised file creation date

--- Program.cs
+++ Program.cs
@@ -1,33 +1,83 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+
 Console.WriteLine("Hello, World!");
 
 int FOLDER_COUNT_MAX = 100;
 int FILE_COUNT_MAX = 100;
 string[] extensions = new[] { "XLSX", "DOCX", "TXT", "SQL", "PDF", "CS" };
 
-var path = Directory.GetCurrentDirectory();
+
+var path = Path.Combine(Directory.GetCurrentDirectory(), "files");
+
+#if DEBUG
+// auto cleanup for debugging!
+Directory.Delete(path, true);
+#endif
+
 var random = new Random();
 
 for (var folderCount = 0; folderCount < FOLDER_COUNT_MAX; folderCount++)
 {
-    var directory = Path.Combine(path, Guid.NewGuid().ToString());
-    Directory.CreateDirectory(directory);
+    var directory = Directory.CreateDirectory(GenerateFolderPath(path)).ToString();
 
-    var numberOfFiles = random.Next(FILE_COUNT_MAX);
+    CreateFilesForFolder(directory);
+}
 
+void CreateFilesForFolder(string folderPath, bool createNested = false)
+{
+    var nested = random.Next(2) == 0;
+    if (nested)
+    {
+        int nestedFolders = random.Next(0, 15);
+        int counter = 0;
+        while (counter < nestedFolders)
+        {
+            var path = CreateDirectory(folderPath);
+            GenerateFiles(path, random.Next(FILE_COUNT_MAX));
+            counter++;
+        }
+
+    }
+
+    else
+    {
+        GenerateFiles(folderPath, random.Next(FILE_COUNT_MAX));
+    }
+
+
+}
+
+void GenerateFiles(string folderPath, int numberOfFiles)
+{
     for (var fileCount = 0; fileCount < numberOfFiles; fileCount++)
     {
+        var days = random.Next(1, 100);
+        DateTime creationTime = DateTime.Now.AddDays(-random.Next(1, 100)).AddHours(random.Next(1, 100)).AddMinutes(random.Next(1, 100)); // randomise the file creation date anywhere from now to 100 days ago
         Console.WriteLine($"Creating {numberOfFiles}");
         var extensionRandomiser = extensions[random.Next(extensions.Length)];
 
         var newFileName = $"{Guid.NewGuid()}.{extensionRandomiser}";
 
-        using (File.Create(Path.Combine(directory, newFileName)))
+        using (var file = File.Create(Path.Combine(folderPath, newFileName)))
         {
-
         }
 
-        Console.WriteLine($"Create file {newFileName} in directory {directory}");
+        var filePath = Path.Combine(folderPath, newFileName);
+        FileStream fs = new FileStream(filePath, FileMode.Create);
+        fs.Close();
+
+        File.SetCreationTime(filePath, creationTime);
+
+        Console.WriteLine($"Create file {newFileName} in directory {folderPath} with a date of {creationTime}");
 
     }
 }
+
+string GenerateFolderPath(string path)
+{
+    var newPath = Path.Combine(path, Guid.NewGuid().ToString());
+    return newPath.ToString();
+}
+
+string CreateDirectory(string name) => Directory.CreateDirectory(Path.Combine(name, Guid.NewGuid().ToString()).ToString()).ToString();
