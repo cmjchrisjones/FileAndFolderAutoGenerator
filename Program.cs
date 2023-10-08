d0018e49f7a001b5ed22725c76cb27f94adcdb28
Chris Jones <cmj.chris.jones@googlemail.com>
2023-10-09 00:05:00 +0100

    Added a bit more readme and also added a summary message and stopwatch

--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
 using System.IO;
 
 Console.WriteLine("Hello, World!");
+var stopwatch = new Stopwatch();
+stopwatch.Start();
 
-int FOLDER_COUNT_MAX = 100;
-int FILE_COUNT_MAX = 100;
+const int FOLDER_COUNT_MAX = 100;
+const int FILE_COUNT_MAX = 100;
 string[] extensions = new[] { "XLSX", "DOCX", "TXT", "SQL", "PDF", "CS" };
+int totalFiles = 0;
+int totalFolders = 0;
 
 
 var path = Path.Combine(Directory.GetCurrentDirectory(), "files");
 
 #if DEBUG
 // auto cleanup for debugging!
-Directory.Delete(path, true);
+if (Directory.Exists(path))
+{
+    Directory.Delete(path, true);
+}
 #endif
 
 var random = new Random();
@@ -20,9 +28,12 @@ var random = new Random();
 for (var folderCount = 0; folderCount < FOLDER_COUNT_MAX; folderCount++)
 {
     var directory = Directory.CreateDirectory(GenerateFolderPath(path)).ToString();
-
+    totalFolders++;
     CreateFilesForFolder(directory);
 }
+
+stopwatch.Stop();
+Console.WriteLine($"Created a total of {totalFolders} folders with a combined {totalFiles} files in {stopwatch.Elapsed}");
 
 void CreateFilesForFolder(string folderPath, bool createNested = false)
 {
@@ -54,7 +65,6 @@ void GenerateFiles(string folderPath, int numberOfFiles)
     {
         var days = random.Next(1, 100);
         DateTime creationTime = DateTime.Now.AddDays(-random.Next(1, 100)).AddHours(random.Next(1, 100)).AddMinutes(random.Next(1, 100)); // randomise the file creation date anywhere from now to 100 days ago
-        Console.WriteLine($"Creating {numberOfFiles}");
         var extensionRandomiser = extensions[random.Next(extensions.Length)];
 
         var newFileName = $"{Guid.NewGuid()}.{extensionRandomiser}";
@@ -69,7 +79,8 @@ void GenerateFiles(string folderPath, int numberOfFiles)
 
         File.SetCreationTime(filePath, creationTime);
 
-        Console.WriteLine($"Create file {newFileName} in directory {folderPath} with a date of {creationTime}");
+        Console.WriteLine($"Created file {newFileName} in directory {folderPath} with a date of {creationTime}");
+        totalFiles++;
 
     }
 }
@@ -80,4 +91,8 @@ string GenerateFolderPath(string path)
     return newPath.ToString();
 }
 
-string CreateDirectory(string name) => Directory.CreateDirectory(Path.Combine(name, Guid.NewGuid().ToString()).ToString()).ToString();
+string CreateDirectory(string name)
+{
+    totalFolders++;
+    return Directory.CreateDirectory(Path.Combine(name, Guid.NewGuid().ToString()).ToString()).ToString();
+}
--- README.md
+++ README.md
@@ -10,11 +10,19 @@ By default, when the program is run, it will create a `files` directory in the `
 
 When run in debug mode, it will automatically delete the `files` directory and recreate it in the name of housekeeping!
 
+You can change the amount of files per folder, and number of root folders it produces by changing the `FOLDER_COUNT_MAX` and `FILE_COUNT_MAX` constants.
 The project requires the .NET 8 runtime.
 
 You are free to do whatever you like with this without licence, but there is no warranty and I can not be held liable for whatever stupid things you decide to try and do with it!
 
+An example of how wild it can be, it takes just 27.5 seconds to generate 18,754 files across 436 folders!
+
+
+![image](https://github.com/cmjchrisjones/FileAndFolderAutoGenerator/assets/3969086/24f07767-dfb3-4caa-97b5-5eed8ae59677)
+
+
 ![image](https://github.com/cmjchrisjones/FileAndFolderAutoGenerator/assets/3969086/f3ae8d44-3b01-475f-9d4f-0dbf41d2d2ec)
+
 
 ![image](https://github.com/cmjchrisjones/FileAndFolderAutoGenerator/assets/3969086/820b1705-65f2-448c-bcf5-d5cf50baa647)
 
