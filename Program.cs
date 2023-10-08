a7d769f01ef91b9fe17d06f756c8c567cb382873
Chris Jones <cmj.chris.jones@googlemail.com>
2023-10-08 22:18:04 +0100

    Applying ChatGPT suggestions

--- Program.cs
+++ Program.cs
@@ -3,27 +3,29 @@ Console.WriteLine("Hello, World!");
 
 int FOLDER_COUNT_MAX = 100;
 int FILE_COUNT_MAX = 100;
-string[] extensions = new[] { "XLSX", "DOCX", "TXT", "SQL" };
+string[] extensions = new[] { "XLSX", "DOCX", "TXT", "SQL", "PDF", "CS" };
 
 var path = Directory.GetCurrentDirectory();
+var random = new Random();
 
 for (var folderCount = 0; folderCount < FOLDER_COUNT_MAX; folderCount++)
 {
     var directory = Path.Combine(path, Guid.NewGuid().ToString());
     Directory.CreateDirectory(directory);
 
-    var numberOfFiles = new Random().Next(FILE_COUNT_MAX);
+    var numberOfFiles = random.Next(FILE_COUNT_MAX);
 
     for (var fileCount = 0; fileCount < numberOfFiles; fileCount++)
     {
         Console.WriteLine($"Creating {numberOfFiles}");
-        var fileName = Guid.NewGuid().ToString();
-        var extenionRandomiser = new Random().Next(extensions.Length);
-        var extension = extensions[extenionRandomiser];
+        var extensionRandomiser = extensions[random.Next(extensions.Length)];
 
-        var newFileName = $"{fileName}.{extension}";
+        var newFileName = $"{Guid.NewGuid()}.{extensionRandomiser}";
 
-        File.Create(Path.Combine(directory, newFileName));
+        using (File.Create(Path.Combine(directory, newFileName)))
+        {
+
+        }
 
         Console.WriteLine($"Create file {newFileName} in directory {directory}");
 
